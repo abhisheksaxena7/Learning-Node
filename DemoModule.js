c6b48a35c1ba095a8a3b9c7ca369a96f17222139
Abhishek Saxena <abhisheksaxena2.12@gmail.com>
2017-12-03 18:13:51 +0530

    Read File example and changed directory structure

--- DemoModule.js
+++ DemoModule.js
@@ -1,5 +1,5 @@
 var http = require('http');
-var dt = require('./MyFirstModule');
+var dt = require('./Modules/MyFirstModule');
 
 http.createServer(function(req, res){
 	res.writeHead(200,{'Content-Type': 'text/html'});
