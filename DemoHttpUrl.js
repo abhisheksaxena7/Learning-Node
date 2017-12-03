721bfa13c8e5b0b7789271c582051e745a710924
Abhishek Saxena <abhisheksaxena2.12@gmail.com>
2017-12-03 14:57:23 +0530

    Examples for HTTP Module

---
+++ DemoHttpUrl.js
@@ -0,0 +1,8 @@
+//Try with URL : http://localhost:8080/winter and http://localhost:8080/summer
+var http = require('http');
+
+http.createServer(function (req, res){
+	res.writeHead(200, {'Content-Type': 'text-html'});
+	res.write(req.url);
+	res.end();
+}).listen(8080);
