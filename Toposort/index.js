af9adaa9479f3b2e95e6067f8025d54123b41d43
Abhishek Saxena <abhisheksaxena2.12@gmail.com>
2018-05-07 17:49:31 +0530

    Another example using topo-sort.

--- Toposort/index.js
+++ Toposort/index.js
@@ -1,24 +1,28 @@
+topo_sort = require('topo-sort');
 toposort = require('toposort');
 
-// This time, edges represent dependencies.
-var graph = [
-    ['tie your shoes', 'put on your shoes'],
-    ['put on your jacket', 'put on your shirt'],
-    ['put on your shoes', 'put on your shorts'],
-    ['put on your jacket', 'put on your shorts']
-]
+var tsort = new topo_sort();
+tsort.add('bl', 'schema');
+tsort.add('ui', ['schema', 'bl']);
+tsort.add('perms', ['schema', 'bl', 'ui']);
+//Order using Topo - Sort schema, bl, ui, perms
+console.log('Right Order using Topo-Sort ' + tsort.sort().reverse());
 
-toposort(graph);
-// [ 'tie your shoes'
-// , 'put on your shoes'
-// , 'put on your jacket'
-// , 'put on your shirt'
-// , 'put on your shorts' ]
+var sfdx = [
+    ['bl', 'schema'],
+    ['ui', 'schema'],
+    ['ui', 'bl'],
+    ['perms', 'schema'],
+    ['perms', 'bl'],
+    ['perms', 'ui']
+];
+//Right Order using Toposort schema,bl,ui,perms
+console.log('Right Order using Toposort ' + toposort(sfdx).reverse());
 
-// Now, reversing the list will reveal a legal execution order.
-toposort(graph).reverse();
-// [ 'put on your shorts'
-// , 'put on your shirt'
-// , 'put on your jacket'
-// , 'put on your shoes'
-// , 'tie your shoes' ]
+var sfdxMultiArray = [
+    ['bl', 'schema'],
+    ['ui', 'schema', 'bl'],
+    ['perms', 'schema', 'bl', 'ui'],
+];
+//Wrong order using Toposort schema, perms, ui, bl
+console.log('Wrong order using Toposort ' + toposort(sfdxMultiArray).reverse());
