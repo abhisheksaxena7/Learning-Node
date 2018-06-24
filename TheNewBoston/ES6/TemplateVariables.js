6deae9ba075b8e5c3d3b6313010bab94bd144ea3
Abhishek Saxena <abhisheksaxena2.12@gmail.com>
2018-06-24 14:42:50 +0530

    ES6 Template Variables.

---
+++ TheNewBoston/ES6/TemplateVariables.js
@@ -0,0 +1,12 @@
+const name = 'Abhishek';
+
+console.log('I like ' + name + ' because he smells good.');
+console.log(`I like ${name} because he smells good.`);
+
+let a = 5;
+let b = 7;
+
+console.log(`I like ${a + b} because he smells good.`);
+
+console.log(`I like ${name} because 
+he smells good.`);
