110f9fdd48dcb7e346a9e2ad4dd10003a919aa77
Abhishek Saxena <abhisheksaxena2.12@gmail.com>
2018-06-24 15:31:29 +0530

    added padded-blocks config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,5 +3,8 @@ module.exports = {
     rules: {
         "no-console": 0,
         "indent": ["error", 4],
+        "padded-blocks": ["error", {
+            "classes": "always"
+        }]
     }
 };
