1520d51615be9996a11e0cc2f8a5fb886364025f
Abhishek Saxena <abhisheksaxena2.12@gmail.com>
2018-07-17 12:30:22 +0530

    Wrote gulp tasks to simplify symlinking.

---
+++ SymLinking/gulpfile.js
@@ -0,0 +1,31 @@
+// Sample command run:
+// gulp update:symlink 
+// -d /Users/appirio-13524/Documents/work-repos/node-dx/
+// -a /Users/appirio-13524/Documents/work-repos/node-appirio/
+// -s /Users/appirio-13524/Documents/work-repos/node-salesforce
+
+const gulp = require('gulp');
+const {
+    argv,
+} = require('yargs');
+const index = require('./index');
+
+const nodeDx = argv.d;
+const nodeAppirio = argv.a;
+const nodeSalesforce = argv.s;
+const arrayNode = [nodeDx, nodeAppirio, nodeSalesforce];
+
+gulp.task('symlink', () => {
+    index.npmLink(arrayNode);
+});
+
+// This task updates the 3 Node-repositories
+// and creates updated symlinks.
+gulp.task('update:symlink', () => {
+    index.updateRepos(arrayNode);
+    index.deleteExistingDirs(arrayNode);
+    index.installNodeModules(arrayNode);
+    index.linkNodes(nodeSalesforce, nodeDx);
+});
+
+gulp.task('create:symlink', ['symlink', 'update:symlink']);
---
+++ SymLinking/index.js
@@ -0,0 +1,75 @@
+const fs = require('fs-extra');
+const path = require('path');
+const {
+    spawnSync,
+} = require('child_process');
+
+const npmLink = (arrayNode) => {
+    arrayNode.forEach((node) => {
+        spawnSync('npm', ['link'], {
+            shell: true,
+            stdio: 'inherit',
+            cwd: node,
+        });
+    });
+}
+
+const updateRepos = (arrayNode) => {
+    arrayNode.forEach((node) => {
+        spawnSync('git', ['checkout', 'master'], {
+            shell: true,
+            stdio: 'inherit',
+            cwd: node,
+        });
+        spawnSync('git', ['pull', 'origin', 'master'], {
+            shell: true,
+            stdio: 'inherit',
+            cwd: node,
+        });
+    });
+}
+
+const deleteExistingDirs = (arrayNode) => {
+    arrayNode.forEach((node) => {
+        const pathToPackageLock = path.join(node, 'package-lock.json');
+        const pathToNodeModules = path.join(node, 'node_modules');
+        fs.removeSync(pathToPackageLock);
+        fs.removeSync(pathToNodeModules);
+    });
+};
+
+const installNodeModules = (arrayNode) => {
+    arrayNode.forEach((node) => {
+        spawnSync('npm', ['install'], {
+            shell: true,
+            stdio: 'inherit',
+            cwd: node,
+        });
+    });
+};
+
+const linkNodes = (nodeSalesforce, nodeDx) => {
+    spawnSync('npm', ['link', '@appirio/appirio'], {
+        shell: true,
+        stdio: 'inherit',
+        cwd: nodeSalesforce,
+    });
+    spawnSync('npm', ['link', '@appirio/appirio'], {
+        shell: true,
+        stdio: 'inherit',
+        cwd: nodeDx,
+    });
+    spawnSync('npm', ['link', '@appirio/salesforce'], {
+        shell: true,
+        stdio: 'inherit',
+        cwd: nodeDx,
+    });
+};
+
+module.exports = {
+    npmLink,
+    updateRepos,
+    deleteExistingDirs,
+    installNodeModules,
+    linkNodes,
+};
