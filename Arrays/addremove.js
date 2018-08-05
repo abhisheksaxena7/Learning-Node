991583fe8aa83f305c07e15852e02022ef9bd7f5
Abhishek Saxena <abhisheksaxena2.12@gmail.com>
2018-08-05 13:36:02 +0530

    Adding and removing stuff from an Array.

---
+++ Arrays/addremove.js
@@ -0,0 +1,17 @@
+const characters = ['Klaus', 'Elijah', 'Rebekkah', 'Kol'];
+
+characters.push('Hayley');
+characters.push('Hope');
+console.log(characters);
+
+characters.pop();
+characters.pop();
+console.log(characters);
+
+characters.unshift('Mikael');
+characters.unshift('Esther');
+console.log(characters);
+
+characters.shift();
+characters.shift();
+console.log(characters);
