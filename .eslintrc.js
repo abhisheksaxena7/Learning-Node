f49273e76496d9a3127cc11dff59aa59413b659b
Abhishek Saxena <abhisheksaxena2.12@gmail.com>
2018-08-17 18:32:52 +0530

    Basic jest matchers

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,10 @@
 module.exports = {
     "extends": "airbnb-base",
+    "env": {
+        "es6": true,
+        "node": true,
+        "jest": true
+    },
     rules: {
         "no-console": 0,
         "indent": ["error", 2],
---
+++ jest/__tests__/functions.test.js
@@ -0,0 +1,52 @@
+const functions = require('../functions');
+
+test('Adds 2 + 2 to equal 4', () => {
+  expect(functions.add(2, 2)).toBe(4);
+});
+
+test('Adds 2 + 2 to not equal 5', () => {
+  expect(functions.add(2, 2)).not.toBe(5);
+});
+
+// CHECK FOR THRUTHY & FALSY VALUES
+// toBeNull matches only Null
+// toBeUndefined matches only undefined
+// toBeDefined is the opposite of toBeUndefined
+// toBeTruthy matches anything that an if statement treats as true
+// toBeFalsy matches anything that an if statement treats as false
+
+test('Should be null', () => {
+  expect(functions.isNull()).toBeNull();
+});
+
+test('Should be falsy', () => {
+  expect(functions.checkValue(null)).toBeFalsy();
+  expect(functions.checkValue(0)).toBeFalsy();
+  expect(functions.checkValue(undefined)).toBeFalsy();
+  expect(functions.checkValue(false)).toBeFalsy();
+});
+
+test('User should be Abhishek Saxena', () => {
+  // expect(functions.createUser()).toBe({   ----- This WILL FAIL since Objects are reference type.
+  expect(functions.createUser()).toEqual({
+    firstName: 'Abhishek',
+    lastName: 'Saxena',
+  });
+});
+
+test('Should be under 1600', () => {
+  const num1 = 800;
+  const num2 = 700;
+  expect(num1 + num2).toBeLessThanOrEqual(1600);
+});
+
+// Regex
+test('There is no I in team', () => {
+  expect('team').not.toMatch(/I/);
+});
+
+// Arrays
+test('Admin should be in usernames', () => {
+  const usernames = ['Andrew', 'Ashna', 'Saurabh'];
+  expect(usernames).toContain('Andrew');
+});
