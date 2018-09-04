d95505786f4eef405c1896e873ab2032f6e27c5a
Abhishek Saxena <abhisheksaxena2.12@gmail.com>
2018-09-04 15:32:56 +0530

    Node stream pipes

--- Streams/streams.js
+++ Streams/streams.js
@@ -5,7 +5,12 @@ const myReadStream = fs.createReadStream(`${__dirname}/dataToBeRead.txt`, 'utf-8
 
 const myWriteStream = fs.createWriteStream(`${__dirname}/dataToBeWritten.txt`);
 
-myReadStream.on('data', (chunk) => {
-  console.log('Received chunk');
-  myWriteStream.write(chunk);
-});
+// myReadStream.on('data', (chunk) => {
+//   console.log('Received chunk');
+//   myWriteStream.write(chunk);
+// });
+
+// Pipe function - takes data from a read stream and pipe it to a write stream
+// So we do not have to listen to data even and don't have to manually write to
+// a write stream.
+myReadStream.pipe(myWriteStream);
