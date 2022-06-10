acc067d2bf885fdfc99344f33c033019849b11af
Abhishek Saxena <abhisheksaxena2.12@gmail.com>
2022-06-09 23:29:14 -0400

    Temp commit

--- Streams/split-ex1.js
+++ Streams/split-ex1.js
@@ -6,10 +6,10 @@ const myReadStream = fs.createReadStream(`${__dirname}/dataToBeRead.txt`, 'utf-8
 
 let i = 0;
 myReadStream
-  // .pipe(split2())
+  .pipe(split2())
   .on('data', (line) => {
     // each chunk now is a separate line!
     console.log('Received chunk');
-    // console.log(line);
+    console.log(line);
     console.log(i++);
   });
